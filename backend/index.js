5926f95e7a2dfb9fd59d39fa5c02c16722ce8818
Amrindra <amrindraouk@yahoo.com>
2022-03-15 16:32:40 -0400

    setup database with mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,34 @@
 const PORT = 8000;
 
 const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://amrin:tinder-12345@cluster0.9bbug.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.json("Hello");
+});
+
+app.post("/signup", (req, res) => {
+  res.json("signup");
+});
+
+app.get("/users", async (req, res) => {
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    const database = client.db("app-data");
+    const users = database.collection("users");
+
+    const returnUsers = await users.find().toArray();
+    res.send(returnUsers);
+  } finally {
+    await client.close();
+  }
+});
 
 app.listen(PORT, () => console.log(`Server running port ${PORT}`));
